ce15bcb0ab73bb0ed95c823cc92875aca68f34b6
Naveira <manuelnaveira7@gmail.com>
2023-10-23 22:32:59 +0200

    Primer commit: proyecto inicial

---
+++ src/main/java/com/gestorEconomico/gestorEconomiaFamiliar/GestorEconomiaFamiliarApplication.java
@@ -0,0 +1,13 @@
+package com.gestorEconomico.gestorEconomiaFamiliar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorEconomiaFamiliarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorEconomiaFamiliarApplication.class, args);
+	}
+
+}
